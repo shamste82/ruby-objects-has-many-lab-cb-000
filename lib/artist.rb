518b85fd09fa01ac6eb758d7c38fd1a8cbefd9c6
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-05 04:57:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 class Artist
   attr_accessor :name, :songs
-
+  @@song_count = 0
+  
   def initialize(name)
     @name = name
     @songs = Array.new
-    @@song_count = 0
   end
 
   def add_song(song)

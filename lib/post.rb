604d93366ef9bd9f86ae3da59f75726adf4e5105
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-05 05:49:50 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialize(name)
+    @title = name
+  end
+
+  def author_name
+    if @author
+      @author.name
+    else
+      nil
+    end
+  end
+
+end
